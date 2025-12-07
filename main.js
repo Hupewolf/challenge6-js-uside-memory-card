2d9c780281f8deb572288f102b5b351bbf1fc3d0
Hupewolf <nhannguyen.ix5@gmail.com>
2025-12-08 00:32:14 +0700

    test

--- main.js
+++ main.js
@@ -94,7 +94,7 @@ emojis.forEach(emoji => {
                     card.classList.remove("flipped");
                     firstCard = null;
                     lock = false;
-                }, 800);
+                }, 200);
             }
         }
     });
@@ -178,7 +178,7 @@ function resetGame() {
                         card.classList.remove("flipped");
                         firstCard = null;
                         lock = false;
-                    }, 800);
+                    }, 200);
                 }
             }
         });
